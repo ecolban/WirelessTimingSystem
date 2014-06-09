6c2058b4bbb1753f7c3909c0f5644502b2dc27a6
Erik Colban <erik.colban@drawmetry.com>
2014-06-08 22:39:31 -0700

    Added manual mode

--- WirelessTimingSystem/src/org/wintrisstech/erik/iaroc2014/WTS_Timer.java
+++ WirelessTimingSystem/src/org/wintrisstech/erik/iaroc2014/WTS_Timer.java
@@ -5,6 +5,9 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
@@ -12,36 +15,65 @@ import java.net.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.Timer;
+import javax.swing.border.CompoundBorder;
 
 @SuppressWarnings("serial")
 public class WTS_Timer extends JPanel implements Runnable, ActionListener {
+	private static final String HOST_PREFIX = "192.168.42.";
 	private static final int SERVER_PORT = 47047;
-	private long start;
-	private long now;
+	private static final Pattern EVENT_PATTERN = Pattern.compile("(local|remote):(high|low)");
+	private static final String LOCAL = "local";
+	private static final String REMOTE = "remote";
+	private static final String HIGH = "high";
+//	private static final String LOW = "low";
+	private static final String RESET = "RESET";
+	private static final String START = "START";
+	private static final String STOP = "STOP";
+	private static final String RESUME = "RESUME";
+	private static final String NO_TEXT = "---";
+	private static final Font displayFont = new Font("Helvetica", Font.PLAIN, 144);
+
+	private String id; // "1" or "2" to identify RPi-1 or RPi-2.
 	private Timer ticker = new Timer(20, this);
-	Font displayFont = new Font("Helvetica", Font.PLAIN, 144);
-	private boolean running = false;
-	private boolean armed = false;
+	// beam states
 	private boolean localHigh = false;
 	private boolean remoteHigh = false;
-	private PrintWriter out;
-	private static final Pattern EVENT_PATTERN = Pattern.compile("(local|remote):(high|low)");
+	// timer states
+	// 3 states, each identified by two boolean variables:
+	//  1) armed: armed && !running
+	//  2) running: !armed && running
+	//  3) stopped: !armed && !running
+	
+	private boolean running = false;
+	private boolean armed = true;
+	//
+	private long start; // Time of transition from armed to running state
+	private boolean manualMode = false; // If true, manual mode, else automatic mode.
+	private JButton armButton;
+	private JButton manualControlButton;
+	private JTextField timeTextField;
+
+	public WTS_Timer(String id) {
+		this.id = id;
+	}
 
 	public static void main(String[] args) throws IOException {
 		if (args.length != 1 || !args[0].matches("[1-2]")) {
 			printUsage();
 			System.exit(1);
 		}
-		String host = "192.168.42." + args[0];
 		// String host = "localhost";
-		WTS_Timer wtsTimer = new WTS_Timer();
+		WTS_Timer wtsTimer = new WTS_Timer(args[0]);
 		SwingUtilities.invokeLater(wtsTimer);
-		wtsTimer.listen(host, SERVER_PORT);
+		wtsTimer.listen(HOST_PREFIX + args[0], SERVER_PORT);
 	}
 
 	private static void printUsage() {
@@ -56,7 +88,6 @@ public class WTS_Timer extends JPanel implements Runnable, ActionListener {
 			System.out.println("Trying to connect to " + host + ":" + serverPort);
 			socket = new Socket(host, serverPort);
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			out = new PrintWriter(socket.getOutputStream(), true);
 			System.out.println("Connected to " + socket.getRemoteSocketAddress());
 			boolean listening = true;
 			String line;
@@ -68,8 +99,8 @@ public class WTS_Timer extends JPanel implements Runnable, ActionListener {
 					Matcher m = EVENT_PATTERN.matcher(line);
 					if (m.matches()) {
 						String source = m.group(1);
-						String state = m.group(2);
-						changeState(source, state);
+						String beamState = m.group(2);
+						changeState(source, beamState);
 					}
 				}
 			}
@@ -83,8 +114,6 @@ public class WTS_Timer extends JPanel implements Runnable, ActionListener {
 					socket.close();
 				if (in != null)
 					in.close();
-				if (out != null)
-					out.close();
 			} catch (IOException e) {
 				System.out.println(e.getMessage());
 			}
@@ -94,37 +123,72 @@ public class WTS_Timer extends JPanel implements Runnable, ActionListener {
 
 	@Override
 	public void run() {
-		JFrame frame = new JFrame("EventClient");
-		setPreferredSize(new Dimension(600, 184));
+		JFrame frame = new JFrame("Timer Lane " + id);
+		setPreferredSize(new Dimension(600, 20));
 		frame.setLayout(new BorderLayout());
-		frame.add(this, BorderLayout.CENTER);
+		frame.add(this, BorderLayout.NORTH);
+		timeTextField = new JTextField("00:00.00");
+		CompoundBorder border = BorderFactory.createCompoundBorder(
+				BorderFactory.createEmptyBorder(),
+				BorderFactory.createEmptyBorder(0, 20, 0, 0));
+		timeTextField.setBorder(border);
+		timeTextField.setFont(displayFont);
+		frame.add(timeTextField, BorderLayout.CENTER);
 		JPanel buttonPanel = new JPanel();
-		// Reset button
-		JButton startButton = new JButton("START");
-		startButton.addActionListener(new ActionListener() {
+		// Arm button
+		armButton = new JButton(RESET);
+		armButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				armed = true;
-				running = false;
-				start = now;
+				reset();
+				armButton.setSelected(true);
 			}
 		});
-		buttonPanel.add(startButton);
-
-		// Resume button
-		final JButton resumeButton = new JButton("Resume");
-		resumeButton.addActionListener(new ActionListener() {
+		armButton.setSelected(armed);
+		buttonPanel.add(armButton);
+		// Manual radio button
+		final JRadioButton manualRadioButton = new JRadioButton();
+		manualRadioButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (!armed && remoteHigh) {
-					running = true;
+				manualMode = manualRadioButton.isSelected();
+				if (manualMode) {
+					if (armed) {
+						manualControlButton.setText(START);
+					} else if (running) {
+						manualControlButton.setText(STOP);
+					} else {
+						manualControlButton.setText(RESUME);
+					}
+				} else {
+					manualControlButton.setText(NO_TEXT);
+				}
+				manualControlButton.setEnabled(manualMode);
+			}
+		});
+		manualRadioButton.setSelected(manualMode);
+		buttonPanel.add(manualRadioButton);
+
+		// Manual control button
+		manualControlButton = new JButton(NO_TEXT);
+		manualControlButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (armed) {
+					start();
+				} else if (running) {
+					stop();
+				} else {
+					resume();
 				}
 			}
 		});
+		manualControlButton.setEnabled(manualMode);
+		buttonPanel.add(manualControlButton);
 
-		buttonPanel.add(resumeButton);
 		frame.add(buttonPanel, BorderLayout.SOUTH);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
@@ -133,21 +197,57 @@ public class WTS_Timer extends JPanel implements Runnable, ActionListener {
 		ticker.start();
 	}
 
+	private void reset() {
+		armed = true;
+		running = false;
+		armButton.setSelected(true);
+		if (manualMode) {
+			manualControlButton.setText(START);
+		}
+		timeTextField.setText(getTimeString(start));
+	}
+
+	private void start() {
+		armed = false;
+		start = System.currentTimeMillis();
+		running = true;
+		armButton.setSelected(false);
+		if (manualMode) {
+			manualControlButton.setText(STOP);
+		}
+	}
+
+	private void stop() {
+		running = false;
+		if (manualMode) {
+			manualControlButton.setText(RESUME);
+		}
+	}
+
+	private void resume() {
+		running = true;
+		if (manualMode) {
+			manualControlButton.setText(STOP);
+		}
+	}
+
 	@Override
 	public void paintComponent(Graphics g) {
-		g.setColor(Color.WHITE);
-		g.fillRect(0, 0, getWidth(), getHeight());
-		g.setColor(Color.BLACK);
-		g.setFont(displayFont);
-		g.drawString(getTime(), 20, 144);
-		g.setColor(localHigh ? Color.GREEN : Color.RED);
-		g.drawOval(20, 10, 4, 4);
-		g.setColor(remoteHigh ? Color.GREEN : Color.RED);
-		g.drawOval(30, 10, 4, 4);
+		Graphics2D g2 = (Graphics2D) g;
+		RenderingHints rh = new RenderingHints(
+	             RenderingHints.KEY_TEXT_ANTIALIASING,
+	             RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+	    g2.setRenderingHints(rh);
+		g2.setColor(Color.WHITE);
+		g2.fillRect(0, 0, getWidth(), getHeight());
+		g2.setColor(localHigh ? Color.GREEN : Color.RED);
+		g2.fillOval(20, 10, 8, 8);
+		g2.setColor(remoteHigh ? Color.GREEN : Color.RED);
+		g2.fillOval(34, 10, 8, 8);
 
 	}
 
-	private String getTime() {
+	private String getTimeString(long now) {
 		long hundreds = (now - start + 5) / 10; // round
 		long seconds = hundreds / 100;
 		long minutes = seconds / 60;
@@ -159,23 +259,21 @@ public class WTS_Timer extends JPanel implements Runnable, ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (running) {
-			now = System.currentTimeMillis();
-			repaint();
+			timeTextField.setText(getTimeString(System.currentTimeMillis()));
 		}
 	}
 
-	private void changeState(String source, String state) {
-		if (source.equals("local")) {
-			localHigh = state.equals("high");
-		} else if (source.equals("remote")) {
-			remoteHigh = state.equals("high");
+	private void changeState(String source, String beamState) {
+		if (source.equals(LOCAL)) {
+			localHigh = beamState.equals(HIGH);
+		} else if (source.equals(REMOTE)) {
+			remoteHigh = beamState.equals(HIGH);
 		}
-		if (armed && source.equals("local") && !localHigh) {
-			start = System.currentTimeMillis();
-			running = true;
-			armed = false;
-		} else if (running && source.equals("remote") && !remoteHigh) {
-			running = false;
+		repaint();
+		if (!manualMode && armed && source.equals(LOCAL) && !localHigh) {
+			start();
+		} else if (!manualMode && running && source.equals(REMOTE) && !remoteHigh) {
+			stop();
 		}
 	}
 
